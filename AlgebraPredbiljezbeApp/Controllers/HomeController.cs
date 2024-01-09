eea07b6184099fdca404b4554a0100c28a0b6c2c
Antun Rukavina <56188034+arukavina95@users.noreply.github.com>
2024-01-09 12:19:41 +0100

    Add files via upload

---
+++ AlgebraPredbiljezbeApp/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AlgebraPredbiljezbeApp.Models;
+using AlgebraPredbiljezbeApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlgebraPredbiljezbeApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public string Username { get; set; }
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string naziv)
+        {
+            return View(await _context.Seminar.Where(x => x.Naziv.Contains(naziv) || naziv == null && x.Popunjen == false).ToListAsync());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Odaberi(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seminar = await _context.Seminar.FindAsync(id);
+            if (seminar == null)
+            {
+                return NotFound();
+            }
+
+            Predbiljezba predbiljezba = new Predbiljezba();
+            predbiljezba.SeminarId = id;
+            predbiljezba.Seminar = seminar;
+
+            return View(predbiljezba);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Datum,Ime,Prezime,Adresa,Email,Telefon,SeminarId,Status")] Predbiljezba predbiljezba)
+        {
+            if (ModelState.IsValid)
+            {
+                predbiljezba.Datum = DateTime.Now;
+
+                _context.Add(predbiljezba);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(predbiljezba);
+        }
+    }
+}
---
+++ AlgebraPredbiljezbeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlgebraPredbiljezbeApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public virtual DbSet<Predbiljezba> Predbiljezba { get; set; }
+        public virtual DbSet<Seminar> Seminar { get; set; }
+        public virtual DbSet<Zaposlenik> Zaposlenik { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
